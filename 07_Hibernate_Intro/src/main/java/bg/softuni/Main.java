6ac091219442985d6039349b273479e9f3af83aa
Bankin <bankin@softuni>
2025-06-24 21:48:48 +0300

    Hibernate/JPA Intro

---
+++ 07_Hibernate_Intro/src/main/java/bg/softuni/Main.java
@@ -0,0 +1,28 @@
+package bg.softuni;
+
+import bg.softuni.entities.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+    public static void main(String[] args) {
+        Configuration config = new Configuration();
+        config.configure();
+
+        SessionFactory factory = config.buildSessionFactory();
+        Session currentSession = factory.openSession();
+        currentSession.getTransaction().begin();
+
+//        Student student = new Student();
+//        student.setName("Pesho");
+//
+//        currentSession.persist(student);
+
+        Student found = currentSession.find(Student.class, 1);
+        System.out.println(found.getName());
+
+        currentSession.getTransaction().commit();
+        currentSession.close();
+    }
+}
---
+++ 07_Hibernate_JPA/src/main/java/bg/softuni/entities/Student.java
@@ -0,0 +1,38 @@
+package bg.softuni.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "students_jpa")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
+    private long id;
+
+    @Column(name = "student_name", length = 50)
+    private String name;
+
+    public Student() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
